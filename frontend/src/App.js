faa05c18f400c7edae1c9680ee0f8667dbee7a5f
손덕현 <sondeokhyeon@gmail.com>
2019-07-02 23:07:12 +0900

    Today I Learn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import style from "App.module.scss";
+//import style from "App.module.scss";
 
 function App() {
-  return <div className={style.App} />;
+  return (
+    <div>
+      <h1>Hello CodeSandbox1</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,9 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
+import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
-const middlewares = [thunk];
+
+const history = createBrowserHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
@@ -11,9 +17,24 @@ if (env === "development") {
 }
 
 const reducer = combineReducers({
+  router: connectRouter(history),
   users
 });
 
-let store = initalSate => createStore(reducer, applyMiddleware(...middlewares));
+let store;
+if (env === "development") {
+  store = initialState =>
+    createStore(
+      reducer,
+      compose(
+        applyMiddleware(...middlewares)
+        //Reactotron.createEnhancer()
+      )
+    );
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+export { history };
 
 export default store();
