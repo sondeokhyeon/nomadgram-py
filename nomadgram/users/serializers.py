a06d95fb711d02aa8b6e0ad93097bede0cc43825
손덕현 <sondeokhyeon@gmail.com>
2019-08-21 16:52:31 +0900

    Today I Learn

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -31,14 +31,25 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 class ListUserSerialzer(serializers.ModelSerializer):
+
+    following = serializers.SerializerMethodField()
+
     class Meta:
         model = models.User
         fields = (
             'id',
             'profile_image',
             'username',
-            'name'
+            'name',
+            'following'
         )
+
+    def get_following(self, obj):
+        if 'request' in self.context:
+            request = self.context['request']
+            if obj in request.user.following.all():
+                return True
+        return False
 
 
 class SignUpSerializer(RegisterSerializer):
